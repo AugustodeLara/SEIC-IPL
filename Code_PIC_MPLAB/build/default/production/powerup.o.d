c4be81291b150c0e612ab3abfa0587e8630636fc
aglara <augusto.lara@acad.pucrs.br>
2022-09-13 23:44:59 -0300

    remove files no used in code_pic

--- Code_PIC_MPLAB/build/default/production/powerup.o.d
+++
@@ -1,7 +0,0 @@
- build/default/production/powerup.d  \
- build/default/production/powerup.obj:  \
- powerup.as  \
-C:/Program\ Files/Microchip/MPLABX/v6.00/packs/Microchip/PIC18F-K_DFP/1.5.114/xc8/pic/include/pic18_chip_select.inc  \
-C:/Program\ Files/Microchip/MPLABX/v6.00/packs/Microchip/PIC18F-K_DFP/1.5.114/xc8/pic/include/xc.inc  \
-C:/Program\ Files/Microchip/MPLABX/v6.00/packs/Microchip/PIC18F-K_DFP/1.5.114/xc8/pic/include/pic18.inc  \
-C:/Program\ Files/Microchip/MPLABX/v6.00/packs/Microchip/PIC18F-K_DFP/1.5.114/xc8/pic/include/proc/pic18f26k40.inc 
